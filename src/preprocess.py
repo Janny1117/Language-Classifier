46b6187c97e13b2f6d9a763adc53170db39c6bd6
Janny1117 <gohjiayee1117@gmail.com>
2025-01-07 04:09:32 +0800

    Refactor code for CI pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,5 +12,5 @@ def preprocess_data(input_file):
 # Run the function when the script is executed directly
 if __name__ == "__main__":
     # Ensure the file path is correct (use raw string or double backslashes)
-    data = preprocess_data(r"C:\Users\User\Downloads\MachineLearning\Final Project\Language( Malay & English )_dataset.csv")
+    data = preprocess_data(r"C:\Users\User\Downloads\MachineLearning\project\Language( Malay & English )_dataset.csv")
     print(data.head())  # Print the first few rows of the processed data
